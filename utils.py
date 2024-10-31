7af93f001f6a67049f20e47d364597b18f92c5f9
gbischof <bisc8233@gmail.com>
2024-10-31 16:45:54 -0400

    make get_prop_dir a task

---
+++ utils.py
@@ -0,0 +1,11 @@
+from prefect import task
+from pathlib import Path
+
+@task
+def get_proposal_dir(run):
+    if "Routine Setup and Testing For Beamline 8‐BM".lower() in run.start["proposal"]["title"].lower():
+        proposal_dir = Path(f"/nsls2/data/tes/proposals/commissioning/{run.start['data_session']}")
+    else:
+        proposal_dir = Path(f"/nsls2/data/tes/proposals/{run.start['cycle']}/{run.start['data_session']}")  # noqa: E501
+    
+    return proposal_dir / Path("/Data/{start['operator']}/{dt.date().isoformat()}")
